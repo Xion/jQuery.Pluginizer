91134af18caad6300d78a970649c10d6608ac856
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2011-12-06 23:01:22 +0100

    Support for providing initialData for plugin

--- jquery.pluginizer.js
+++ jquery.pluginizer.js
@@ -7,7 +7,7 @@
 
 (function($) {
 
-    $.pluginize = function(name, methods) {
+    $.pluginize = function(name, methods, initialData) {
         var fn = {};
         fn[name] = function(arg) {
             var rest = [].slice.call(arguments, 1);
@@ -21,7 +21,7 @@
                     var data = (function() {
                         // note that the following relies on fact that object passed to $.data()
                         // is stored by as-is and not copied, because we utilize reference to it directly
-                        var domData  = $this.data(name) || {};
+                        var domData  = $this.data(name) || new Object(initialData);
                         $this.data(name, domData);
                         return domData;
                     })();
